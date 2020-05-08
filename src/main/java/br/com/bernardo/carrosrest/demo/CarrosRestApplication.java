5979bc94151b7179f167c78f40731a1cb8291180
Bernardo <51809748+bgvit@users.noreply.github.com>
2020-05-08 19:44:36 -0300

    Initiate project using Rest API

---
+++ src/main/java/br/com/bernardo/carrosrest/demo/CarrosRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.bernardo.carrosrest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bernardo/carrosrest/demo/CarrosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bernardo.carrosrest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
